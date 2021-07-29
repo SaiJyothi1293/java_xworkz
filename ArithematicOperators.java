1017502510210076fcf41212eae509ad27fc4e29
SaiJyothi1293 <saijyothi.kantamneni@gmail.com>
2021-07-29 16:43:01 +0530

    push ArithematicOperators program

---
+++ ArithematicOperators.java
@@ -0,0 +1,18 @@
+class ArithematicOperators{
+int a=70;
+int b=50;
+int c;
+
+public static void main(String [] args){
+
+ArithematicOperators ao=new ArithematicOperators();
+ao.c=ao.a+ao.b;
+System.out.println("sum of two numbres is="+ao.c);
+ao.c=ao.a-ao.b;
+System.out.println("Difference of two numbers is="+ao.c);
+ao.c=ao.a*ao.b;
+System.out.println("Multiplication of two numbers is="+ao.c);
+ao.c=ao.a%ao.b;
+System.out.println("Modules of two numbers is="+ao.c);
+}
+}
