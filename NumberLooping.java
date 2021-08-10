03333b74a0714e9c47d56d2688f3f1f9fe3749f3
SaiJyothi1293 <saijyothi.kantamneni@gmail.com>
2021-08-10 10:58:02 +0530

    push NumberLooping program

---
+++ NumberLooping.java
@@ -0,0 +1,16 @@
+class NumberLooping
+{
+public static void main(String args[])
+{
+   for (int i=1 ; i<=4 ; i++)
+    {
+        System.out.print(" "+i);
+      }
+    }
+   System.out.println();
+for(int j=5 ; j<=8 ; j++)
+{
+System.out.print(" "+j);
+}
+}
+}
